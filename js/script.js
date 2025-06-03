7a7766d328630ddfc3469018fc1c92ad9510e402
Ahmad Nurfaozi <ahmadnurfaozi4@gmail.com>
2025-06-03 15:21:10 +0700

    add display search

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey: 'bf2e7d792e6d253108d3f660907f9511',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  },
 };
 
 function showSpinner() {
@@ -229,13 +239,31 @@ function displayBackGroundImage(type, backGroundPath) {
 }
 
 async function fetchAPISata(endpoint) {
-  const API_KEY = 'bf2e7d792e6d253108d3f660907f9511';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+
+async function searchAPIDaata(endpoint) {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
   );
 
   const data = await response.json();
@@ -292,6 +320,74 @@ function initSwiper() {
   });
 }
 
+//search function
+async function search() {
+  const queryString = document.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+  global.search.page = urlParams.get('type');
+  if (global.search.term !== '' && global.search.term !== null) {
+    //todo make request
+    const { results, totalPages, page } = await searchAPIDaata();
+    
+    if (results.length === 0) {
+      showAlert('No result found')
+      return
+    }
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value === '';
+
+
+
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
+function displaySearchResults(results){
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+        <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${global.search.type === 'movie'? result.title: result.name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${global.search.type === 'movie'? result.title: result.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie'? result.title: result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie'? result.release_date: result.first_air_date}</small>
+            </p>
+          </div>`;
+    document.querySelector('#search-results').appendChild(div);
+  });
+}
+
+//show alert
+function showAlert(message, className = 'error') {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => {
+    alertEl.remove();
+  }, 3000);
+}
+
 //highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -323,7 +419,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/search.html':
-      console.log('search');
+      search();
       break;
   }
   highlightActiveLink();
